ebf531e2ed1250ada20f49daad5b37d7b3341f2f
sterks <it-saratov@mail.ru>
2014-06-19 17:22:56 +0400

    Add about and bootstrap

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,49 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe "StaticPages", :type => :feature do
-  describe "/static_pages/home" do
-    it "should have the content 'StaticPages#home'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('StaticPages#home')
+  describe "/static_pages" do
+    it "should have the content 'Welcome to the Sample App'" do
+      visit root_path
+      expect(page).to have_content('Welcome to the Sample App')
     end
 
     it "should have the content 'StaticPages#help" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('StaticPages#help')
     end
 
     it "should have the content 'StaticPages#contact" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('StaticPages#contact')
     end
 
+    it "should have the content 'StaticPages#about" do
+      visit about_path
+      expect(page).to have_content('StaticPages#about')
+    end
+
+    it "should have the title 'Ruby on Rails Tutorial Sample App | About" do
+      visit about_path
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+    end
+
     it "should have the title 'Ruby on Rails Tutorial Sample App" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title('Ruby on Rails Tutorial Sample App')
     end
 
     it "should have the title 'Ruby on Rails Tutorial Sample App| Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
     end
 
     it "should have the title 'Ruby on Rails Tutorial Sample App | Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
